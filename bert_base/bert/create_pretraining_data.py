3d0e5f948f8beed1182fe2048272f4fd1c82c038
天地 <qq1274949542@163.com>
2019-04-14 20:49:28 +0800

    limit log

--- bert_base/bert/create_pretraining_data.py
+++ bert_base/bert/create_pretraining_data.py
@@ -144,7 +144,7 @@ def write_instance_to_example_files(instances, tokenizer, max_seq_length,
 
     total_written += 1
 
-    if inst_index < 20:
+    if inst_index < 2:
       tf.logging.info("*** Example ***")
       tf.logging.info("tokens: %s" % " ".join(
           [tokenization.printable_text(x) for x in instance.tokens]))
